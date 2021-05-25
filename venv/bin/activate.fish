9b216b28287ede5683643fc610dd40d401957358
Joe Harkness <joeharkness@Joes-MacBook-Pro.local>
2021-05-25 13:41:32 -0400

    Initial commit

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/joeharkness/Documents/Python_projects/infogol_scaper/venv"
+set -gx VIRTUAL_ENV "/Users/joeharkness/Documents/Python_projects_Github/infogol_scraper/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
